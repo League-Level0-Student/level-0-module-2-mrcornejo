e6a5812a9681dc88980661e6536558c52c7002f9
root <root@4f8b53f09788>
2021-05-22 18:33:26 +0000

    Strings Cannot Add

--- src/_03_string_conversion/_2_strings_cannot_add/StringsCannotAdd.java
+++ src/_03_string_conversion/_2_strings_cannot_add/StringsCannotAdd.java
@@ -1,3 +1,6 @@
+// notes:  is this what you wanted?
+
+
 // hi miranda.  i thin 03_string_conversion-1_intro is just a
 // lesson for you to read, but no code for you to write
 // questions?  you there? sorry i was told to do something but i'm back... okay i just finished reading...
@@ -25,6 +28,9 @@
 // questions?ooooo okay  so now try doing the program below.  i'm going to check on marienne okay
 
 // hi miranda class is over. can you save your work?  next class i hope i can 
+
+// need help saving work?
+
 package _03_string_conversion._2_strings_cannot_add;
 
 import javax.swing.JOptionPane;
@@ -32,23 +38,63 @@ import javax.swing.JOptionPane;
 public class StringsCannotAdd {
 
 		// 1) Make a main method that includes all the steps below…. 
-		
+	public static void main(String[] args) {
+// you worked on some of this already
+	// but it's missing code to "ask the user to enter..."
+	// that means using JOptionPane  want help?yes please
+	
+	
+			String fourAsString = JOptionPane.showInputDialog("enter number 4");
 			// 2) Ask the user to enter number 4 and hold it in the variable fourAsString 
-			int num1 = 4;
+			String sixAsString = JOptionPane.showInputDialog("enter number 6");
 			// 3) Ask the user to enter number 6 and hold it in the variable sixAsString 
-			int num2 = 6;
+			// can you do 3)?  it's like line 43..
+		
+			
 			// 4) Print the addition of fourAsString and sixAsString using JOptionPane 
-			int num3 = num1 + num2;
+		// easier to use System.out.println()
+			System.out.println("glueing strings 4 and 6:");
+			System.out.println(fourAsString + sixAsString);
+// it "GLUED" strings 4 and 6 together	.  yes
+			
 			// Did you notice 46 instead of 4 + 6 = 10?
 			
+			int fourAsInt = Integer.parseInt(fourAsString);
+			int sixAsInt = Integer.parseInt(sixAsString);
+			System.out.println("adding int values 4 and 6:");
+			System.out.println(fourAsInt + sixAsInt);			
+//okay so the first to are lets say the names and the third is calling them to add together? 
+			// one moment
+			// got it?can I copy
+			
+			// steps 5 and 6 will show the same idea
 			// 5) Ask the user to enter number 5 and hold it in the variable fiveAsString 
 			String num4 = "5";
 			// 6) Print the addition of the variables fourAsString, sixAsString, and fiveAsString 
-			int num5 = num1 + num2 + num4;
+		
 			//Did you notice 456 instead of 4 + 6 + 5 = 15?
+
+			// these steps below show that if you really WANT
+			// addition, you need to convert the Strings that
+			// JOptionPane gave you into int values using Integer.parseInt() 
+			// want to try that?
+			// I need to help another student
+
+			// checking in ... questions? no 
+			//am I finished?  up to you.. officially I would try
+			// the steps below to show how you can add 2 int values
+			// instead of gluing them as strings.  what do you think?okay
+			//But which ones do I add?
+			
+			// let's work on the strings you already have above:
+			// fourAsString and sixAsString
+			
+			//int fiveAsString = Integer.parseInt(fiveAsString);
 			
 			// Insert fourAsString inside the parenthesis of Integer.parseInt() and set the method equal to fourAsInt
 			// Hint: int fourAsInt = Integer.parseInt(fourAsString);
+			
+			// can i show you?  class is over soon...
 			
 			// 7) Do the same for sixAsString. 
 			int num6 = 6;
@@ -61,4 +107,5 @@ public class StringsCannotAdd {
 			// 10) Print the addition of fourAsInt, sixAsInt, and fiveAsInt using JOptionPane 
 			
 			// Is your value now 15 instead of 465?
+	}
 }
